e0af5c5c636d777f90f759cdcb70b2a3e5230344
Mariah <110792371+peachysnicker@users.noreply.github.com>
2023-03-27 20:27:00 -0700

    added some comments to existing code

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,5 @@
-const { Schema, model } = require('mongoose');
-const bcrypt = require('bcrypt');
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new Schema(
   {
@@ -12,12 +12,12 @@ const userSchema = new Schema(
       type: String,
       required: true,
       unique: true,
-      match: [/.+@.+\..+/, 'Must use a valid email address'],
+      match: [/.+@.+\..+/, "Must use a valid email address"],
     },
     password: {
       type: String,
       required: true,
-    }
+    },
   },
   {
     toJSON: {
@@ -26,19 +26,20 @@ const userSchema = new Schema(
   }
 );
 
-userSchema.pre('save', async function (next) {
-  if (this.isNew || this.isModified('password')) {
+// Set up pre-save middleware to create password
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
     const saltRounds = 10;
     this.password = await bcrypt.hash(this.password, saltRounds);
   }
   next();
 });
 
+// Compare the incoming password with hashed password
 userSchema.methods.isCorrectPassword = async function (password) {
   return bcrypt.compare(password, this.password);
 };
 
-
-const User = model('User', userSchema);
+const User = model("User", userSchema);
 
 module.exports = User;
