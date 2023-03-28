26efc2dbb2d8006e9c3862e6b24d99a615188dca
Mitsushiro Hirano <mitsushiro1@gmail.com>
2023-03-28 13:34:55 -0400

    changed image files to jpeg

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -22,7 +22,7 @@ db.once('open', async () => {
       title: 'Specialized Rockhopper 27inch',
       description:
         'Specialized Rockhopper in 27 inch rims. Suited for offroad trails.',
-      image: 'specialized-rockhopper-27.5-401456-16.png',
+      image: 'specialized-rockhopper-27.5-401456-16.jpeg',
       category: categories[0]._id,
       price: 2.99,
       quantity: 5
@@ -31,7 +31,7 @@ db.once('open', async () => {
       title: 'Hyperlite 3400 Southwest 55L Backpack - Unisex',
       description:
         'From bushwhacking to tight squeezes, this lightweight pack is designed to go where others falter. The ultra abrasion-resistant Dyneema handles the most rugged adventures while still being light enough for fast missions. Multiple external pockets and daisy chains provide additional storage options on the outside.',
-       image: 'hyperlite-backpack.avif',
+       image: 'hyperlite-backpack.jpeg',
       category: categories[1]._id,
       price: 1.99,
       quantity: 500
@@ -41,7 +41,7 @@ db.once('open', async () => {
       category: categories[2]._id,
       description:
         'Sea to Summit X-Series has been an extremely popular and comprehensive range of collapsible dinnerware for years. The fundamental appeal of these items is how compact and functional they are. Now, Sea to Summit has extended those concepts to cookware with hard-anodized aluminum bases that allow camp stove cooking. The beauty is how they collapse and nest with our dinnerware and other X cookware, creating the most compact cooking and eating sets you will find. There are five set options suited to just about any activity and group size.',
-       image: 'seatosummit-xpot.webp',
+       image: 'seatosummit-xpot.jpeg',
       price: 7.99,
       quantity: 20
     },
@@ -50,7 +50,7 @@ db.once('open', async () => {
       category: categories[3]._id,
       description:
         'This ultralight tent will allow you to push your limits and hike greater distances; whether losing yourself in the beauty of the Pacific Crest Trail, or weathering an afternoon storm in the Swiss Alps. Specifically designed for the demands of long distance backpacking, the award-winning Duplex hits the sweet spot when it comes to size, weight, and features. The Duplex is lightweight, roomy, well-ventilated, packs down small, pitches easily, and can be set up in the tightest of spaces. You won’t find a lighter functional two person tent anywhere on the market. Voted the most popular ultralight tent by Appalachian Trail thru-hikers two years in a row.',
-       image: 'zpacks-duplex-tent.webp',
+       image: 'zpacks-duplex-tent.jpeg',
       price: 3.99,
       quantity: 50
     },
@@ -59,7 +59,7 @@ db.once('open', async () => {
       category: categories[4]._id,
       description:
         'The Quantum Accurist is the rugged baitcast combo needed for your next fishing adventure. By pairing a baitcast reel developed with a ceramic-carbon drag system and a zero-friction design with a highly sensitive IM7 graphite rod, Quantum Accurist was crafted for anglers looking for increased sensitivity and unbeatable durability. With a guaranteed 5-year warranty, the Accurist baitcast combo is a no-brainer.',
-       image: 'quantum-accurist-fishingrod.avif',
+       image: 'quantum-accurist-fishingrod.jpeg',
       price: 14.99,
       quantity: 100
     },
@@ -68,7 +68,7 @@ db.once('open', async () => {
       category: categories[5]._id,
       description:
         'With simplicity born of careful design, the Beta LT delivers breathability and protection. It meets the needs of fast-moving alpinists and skiers in demanding mountain conditions, or anyone looking to strike the right balance between weight and performance. A storm hood, drawcord hem and sealable cuffs are precisely adjustable to make if proof against the elements.',
-       image: 'arcteryx-jacket-mens.avif',
+       image: 'arcteryx-jacket-mens.jpeg',
       price: 399.99,
       quantity: 30
     }
