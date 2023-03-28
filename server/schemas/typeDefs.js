fcb5169e58675f4daa72e2c0e2c82b79343741a7
jhu8480 <118696911+jhu8480@users.noreply.github.com>
2023-03-28 12:11:06 -0400

    adding typeDefs and resolver forpayment

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,12 +1,25 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
+  type Payment {
+    card_number: String
+    expiration_date: String
+    cvv: String
+  }
+
   type User {
     _id: ID
     username: String
     # There is now a field to store the user's password
     email: String
     password: String
+    payment: Payment
+  }
+
+  input PaymentInput {
+    card_number: String!
+    expiration_date: String!
+    cvv: String!
   }
 
   # Set up an Auth type to handle returning data from a profile created or user login
@@ -23,6 +36,9 @@ const typeDefs = gql`
     # Set up mutations to handle creating a profile or logging into a profile and return Auth type
     addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
+
+    # for adding payment info to User profile
+    addPaymentInfo(payment: PaymentInput!): User!
   }
 `;
 
