365a8b9d681cb519cb2a43c854e51e27efd6518a
Mitsushiro Hirano <mitsushiro1@gmail.com>
2023-03-28 11:48:52 -0400

    added categories.js

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,6 @@
 const User = require("./User");
 const Product = require("./Product");
 const Cart = require("./Cart");
+const Category = require('./Category');
 
-module.exports = { User, Product, Cart };
+module.exports = { User, Product, Cart, Category };
